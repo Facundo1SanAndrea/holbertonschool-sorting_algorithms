2559e8eb93600b3e01c7018615103ed8918586b1
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-23 09:00:13 -0700

    Adding sorting algothims of bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ *bubble_sort - sort number from min to max
+ *@array: array
+ *@size: size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size -i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+/**
+ *swap - swaps node
+ *@xp: keep node
+ *@yp: change node
+ */
+
+void swap(int *xp, int *yp)
+{
+	int tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
+}
