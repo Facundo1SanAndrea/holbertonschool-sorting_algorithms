c53de0cb35d3f92add2335b0c88f0e939f219785
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-23 09:22:46 -0700

    Adding selection sort function to the task

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,17 +8,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t step, i = 0;
-	int min_idx = 0;
+	int min_idx, pass = 0;
+
+	if (!array || size == 0)
+		return;
 
 	for (step = 0; step < size - 1; step++)
 	{
 		min_idx = step;
 		for (i = step + 1; i < size; i++)
 		{
+			pass = 0;
 			if (array[i] < array[min_idx])
+			{
 				min_idx = i;
+				pass = 1;
+				swap (&array[min_idx], &array[step]);
+			}
 		}
-		swap (&array[min_idx], &array[step]);
+		if (pass == 0)
+			break;
 	}
 }
 /**
