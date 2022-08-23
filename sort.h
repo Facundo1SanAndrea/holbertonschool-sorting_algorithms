7dfe8610ec8edc70c8e7461afeb02bb7a6bf47ce
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-23 20:35:24 -0300

    Create Insertion Sort function using doubly linked list

--- sort.h
+++ sort.h
@@ -1,5 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #ifndef SORT_H
 #define SORT_H
+
+/* ########### STRUCTURES ############ */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,13 +19,16 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-#include <stdlib.h>
-#include <stdio.h>
+/* ############ PROTOTYPES ############# */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+/* ############ UTILS ################*/
+void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2);
+
 
 #endif
