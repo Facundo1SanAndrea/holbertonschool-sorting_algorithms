775fa170e44807e89dba1748755febcfc04ef261
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-25 18:16:24 -0300

    HotFix - Fix Betty errors and warnings

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-void quick_sort_partion(int *arr, int start, int end, size_t size);
 /**
  * quick_sort - sorts a list of ints order asc(Quick algorithm)
  * @array: array of elements
@@ -14,25 +13,23 @@ void quick_sort(int *array, size_t size)
 	quick_sort_partion(array, 0, size - 1, size);
 }
 
+/**
+ * quick_sort_partion - Recursive func that execute itself based on pivot item
+ * @arr: array of elements
+ * @start: starting index
+ * @end: ending index
+ * @size: amount of elements in array
+ */
 void quick_sort_partion(int *arr, int start, int end, size_t size)
 {
 	int i = start, j = start, pivot = end;
 
-	/*printf("empezo de nuevo\n");*/	
-	
 	while (j <= pivot)
 	{
-		/*printf("i:     <%d> %d\n", i, arr[i]);
-		printf("j:     <%d> %d\n", j, arr[j]);
-		printf("pivot: <%d> %d\n", pivot, arr[pivot]);*/
 		if (arr[j] > arr[pivot])
-		{
-			/*printf("<%d> es > a <%d>\n", arr[j], arr[pivot]);*/
 			j++;
-		}
 		else
 		{
-			/*printf("<%d> es <= a <%d>\n", arr[j], arr[pivot]);*/
 			if (i != j)
 			{
 				swap(&arr[i], &arr[j]);
@@ -41,25 +38,22 @@ void quick_sort_partion(int *arr, int start, int end, size_t size)
 
 			if (j == pivot)
 				break;
-			else
-			{
-				j = i + 1;
-				i = j;
-			}
+
+			j = i + 1;
+			i = j;
 		}
 	}
-	if (((i - 1) - start) > 1)
-	{
-		/*printf("particion de la izquierda\n");*/
+	if ((i - start) > 1)
 		quick_sort_partion(arr, start, i - 1, size);
-	}
-	if ((end - (i - 1)) > 1)
-	{
-		/*printf("particion de la derecha\n");*/
+	if ((end - i) > 1)
 		quick_sort_partion(arr, i + 1, end, size);
-	}
 }
 
+/**
+ *swap - swaps node
+ *@xp: keep node
+ *@yp: change node
+ */
 void swap(int *xp, int *yp)
 {
 	int tmp = *xp;
--- sort.h
+++ sort.h
@@ -20,18 +20,16 @@ typedef struct listint_s
 } listint_t;
 
 /* ############ PROTOTYPES ############# */
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *xp, int *yp);
-
-
 
 /* ############ UTILS ################*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void swap(int *xp, int *yp);
 void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2);
-
+void quick_sort_partion(int *arr, int start, int end, size_t size);
 
 #endif
